e761ef407f69945a2ea53adae7dbb09fb61f7f43
Sourabh <sourabhpoddar102@gmail.com>
2023-02-19 01:22:25 +0530

    demo

--- Sample/src/DemoFile.java
+++ Sample/src/DemoFile.java
@@ -1,7 +1,7 @@
-
+package src;
 public class DemoFile {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello");
+	public static void main(String args[]) {
+		System.out.println();
 	}
 }
