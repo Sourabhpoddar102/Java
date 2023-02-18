5e063426a4a9a9b925ce4da8e62027ab0855d0b8
Sourabh <sourabhpoddar102@gmail.com>
2023-02-19 00:01:30 +0530

    commit after bin delete

--- Sample/src/DemoFile.java
+++ Sample/src/DemoFile.java
@@ -2,7 +2,6 @@
 public class DemoFile {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Github");
-		System.out.println("Second commit");
+		System.out.println("Hello");
 	}
 }
